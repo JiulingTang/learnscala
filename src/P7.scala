b0fdca8e3b9adb5b72b127f2e90db85bf2807129
tang90 <714437428@qq.com>
2017-01-20 13:35:18 -0500

    finish problem 7

--- src/P7.scala
+++ src/P7.scala
@@ -6,5 +6,17 @@ class P7 {
 }
 
 object P7{
-  
+  def f(list:List[Any]):List[Any]= {
+    list match {
+      case Nil => Nil
+      case x :: tail if x.isInstanceOf[List[Any]] => f(x.asInstanceOf[List[Any]]) ::: f(tail)
+      case x :: tail if !x.isInstanceOf[List[Any]] => x :: f(tail)
+    }
+  }
+  def main(args:Array[String])={
+    val l=f(List(List(1,List(2,4,1)),3,List(4)))
+    for (x <- l){
+      print(x+ " ")
+    }
+  }
 }
